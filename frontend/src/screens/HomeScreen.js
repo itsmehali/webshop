577f0f2f185dc9bedd18a3601d0e794c444609f8
itsmehali <halaszbalazs9825@gmail.com>
2021-02-14 18:34:58 +0100

    using useEffect for fecthing data

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await axios.get('/api/products');
+
+      setProducts(data);
+    };
+
+    fetch();
+  }, []);
+
   return (
     <>
       <h1>Products</h1>
