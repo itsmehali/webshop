888683915fea4ddc1d9894c02ca3cfc3163787b2
itsmehali <halaszbalazs9825@gmail.com>
2021-02-15 17:08:25 +0100

    mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
