c4941371d1f7a21fd14b29e9584a16c4682e9198
itsmehali <halaszbalazs9825@gmail.com>
2021-02-24 19:03:40 +0100

    implementing models

---
+++ backend/models/Product.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    description: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
