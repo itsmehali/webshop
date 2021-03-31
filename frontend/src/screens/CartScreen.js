4d8e84d13ae0c5a8ef86c2a8f6d32b61c506765b
itsmehali <halaszbalazs9825@gmail.com>
2021-03-31 13:42:20 +0200

    displaying the cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -7,6 +7,7 @@ import {
   Button,
   Card,
   ListGroup,
+  Container,
 } from 'react-bootstrap';
 import { addToCart } from '../actions/cartActions';
 import { useDispatch, useSelector } from 'react-redux';
@@ -23,6 +24,14 @@ const CartScreen = ({ match, location, history }) => {
   // dispatch for calling the action
   const dispatch = useDispatch();
 
+  // getting data
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
+  const removeItem = id => {
+    console.log('asd');
+  };
+
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
@@ -30,7 +39,47 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
   return (
     <Row>
-      <Col md={6}>Items</Col>
+      <Col md={8}>
+        <h1>Your items</h1>
+        <ListGroup variant='flush'>
+          {cartItems.map(item => (
+            <ListGroup.Item key={item.product}>
+              <Row>
+                <Col md={3}>{item.name}</Col>
+                <Col md={4}>
+                  <Image src={item.image} alt={item.name} fluid />
+                </Col>
+                <Col md={3}>
+                  <Form.Control
+                    as='select'
+                    value={item.qty}
+                    onChange={e =>
+                      dispatch(addToCart(item.product, Number(e.target.value)))
+                    }
+                  >
+                    {[...Array(item.countInStock).keys()].map(i => (
+                      <option key={i + 1} value={i + 1}>
+                        {i + 1}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Col>
+                <Col md={2}>
+                  <h2> {item.price}</h2>
+                  <Button
+                    size='sm'
+                    type='button'
+                    onClick={() => removeItem(item.product)}
+                  >
+                    {' '}
+                    <i className='fas fa-trash'></i>
+                  </Button>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Col>
       <Col md={3}>-</Col>
       <Col md={3}>Total</Col>
     </Row>
