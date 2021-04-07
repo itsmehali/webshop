4f888c07691011204778f20f04c0a83bd799a549
itsmehali <halaszbalazs9825@gmail.com>
2021-04-07 23:01:30 +0200

    implementing to get back the profile

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,6 +2,10 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import asyncHandler from 'express-async-handler';
 
+/*
+@protect: this method defines which route can be protected
+*/
+
 const protect = asyncHandler(async (req, res, next) => {
   let token;
   if (
@@ -20,17 +24,19 @@ const protect = asyncHandler(async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       // put in req.user all of the rest data except the password
-      req.user = await (await User.findById(decoded.id)).isSelected(
-        '-password'
-      );
+      req.user = await User.findById(decoded.id).select('-password');
 
       next();
-    } catch (err) {}
+    } catch (err) {
+      console.error(err);
+      res.status(401);
+      throw new Error('Token has failed, could not authorized!');
+    }
   }
 
   if (!token) {
     res.status(401);
-    throw new Error('Not authorizedn, no token');
+    throw new Error('Not authorized, no token');
   }
 });
 
