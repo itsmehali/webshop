33e88f69f63699762b6259272c7279d7f6750d47
itsmehali <halaszbalazs9825@gmail.com>
2021-04-13 14:36:10 +0200

    LoginScreen skeleton setup

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Button, Form } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+const LoginScreen = () => {
+  return <div></div>;
+};
+
+export default LoginScreen;
